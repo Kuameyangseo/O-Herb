314494b1a16c3453ba288a0eb3f49563abe90e45
Kuameyangseo <iamyangseo@gmail.com>
2025-09-07 03:40:17 +0000

    oherb

---
+++ apps/api-gateway/src/main.ts
@@ -0,0 +1,58 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import express from 'express';
+import cors from 'cors';
+import proxy from "express-http-proxy";
+import swaggerUi from 'swagger-ui-express';
+import axios from 'axios';
+import morgan from 'morgan';
+import cookieparser from 'cookie-parser';  
+import * as path from 'path';
+import rateLimit from 'express-rate-limit';
+
+const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  allowedHeaders: ["Authorization", "content-type"], // Adjust this to your client's origin
+  credentials: true, // Allow cookies to be sent
+}));
+
+app.use(morgan('dev'));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
+app.use(cookieparser());
+app.set('trust proxy', 1); // trust first proxy
+
+// apply rate limit
+const {ipKeyGenerator } = require('express-rate-limit');
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: (req:any) => (req.user ? 1000 :100), // limit each IP to 100 requests per windowMs
+  message: 'Too many requests from this IP, please try again after 15 minutes',
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+   keyGenerator: (req, res) => {
+    return ipKeyGenerator(req.ip);
+  },
+});
+app.use(limiter);
+
+
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.get('/gateway-health', (req, res) => {
+  res.send({ message: 'Welcome to api-gateway!' });
+});
+
+app.use("/", proxy("http://localhost:6001")); // Auth Service
+
+const port = process.env.PORT || 8080;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on('error', console.error);
--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,14 +1,29 @@
 import express from 'express';
-
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+import cors from 'cors';
+import { errorMiddleware } from '../../../packages/error-handler/error-middleware';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  allowedHeaders: ["Authorization", "content-type"], // Adjust this to your client's origin
+  credentials: true, // Allow cookies to be sent
+}));
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send({ 'message': 'Hello API'});
 });
 
-app.listen(port, host, () => {
-    console.log(`[ ready ] http://${host}:${port}`);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 6001;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/`);
+});
+server.on('error', (err) => {
+    console.log("server Error:", err)
 });
