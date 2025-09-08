b9ee0fda1ca79e8295fc67148049e0af68082a90
Kuameyangseo <iamyangseo@gmail.com>
2025-09-08 15:09:05 +0000

    oherb

--- apps/auth-service/src/controller/auth.controller.ts
+++ apps/auth-service/src/controller/auth.controller.ts
@@ -1,11 +1,16 @@
 import { NextFunction, Request, Response } from "express";
-import { validateRegistrationData } from "../utils/auth.helper";
+import { validateRegistrationData, checkOtpRestrictions , trackOtpRequests, sendOtp} from "../utils/auth.helper";
 import prisma from "../../../../packages/libs/prisma";
 import { ValidationError } from "../../../../packages/error-handler";
 
 
-export const userRegistration = async (req:Request, res:Response, next:NextFunction) => {
-    validateRegistrationData(req.body, "user");
+export const userRegistration = async (
+    req:Request, 
+    res:Response, 
+    next:NextFunction
+) => {
+    try{
+         validateRegistrationData(req.body, "user");
     const { name, email} = req.body;
 
     const existingUser = await prisma.users.findUnique({where: email}) // Replace with actual DB check
@@ -14,5 +19,14 @@ export const userRegistration = async (req:Request, res:Response, next:NextFunct
         return next(new ValidationError("user already exists with this email"));
     };
 
-    await checkOtpRestrictions(email,next);
-}
+    await checkOtpRestrictions(email, next);
+    await trackOtpRequests(email, next);
+    await sendOtp(email, name, "user-activation-mail")
+    res.status(200).json({
+        message: "OTP sent to email. please verify your account",
+        success: true
+    });
+} catch (error) {
+        return next(error);
+    }
+};
